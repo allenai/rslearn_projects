2743cdc4921980fc0a61226a43bb9c2d1ee4f462
Favyen Bastani <favyenb@allenai.org>
2024-06-25 12:36:47 -0700

    update configs so they use the right split
    we actually changed the split because md5 was giving bad hashes, like all the hashes
    in some groups were being mapped to val instead of a nice 50-50 split. switched to
    sha256 and it seems better.

---
+++ amazon_conservation/make_dataset/assign_split.py
@@ -0,0 +1,30 @@
+import glob
+import hashlib
+import json
+import os
+import tqdm
+
+ds_root = "/multisat/datasets/rslearn_amazon_conservation_closetime/"
+
+example_id_to_label = {}
+fnames = glob.glob(os.path.join(ds_root, "windows/*/*/layers/label/data.geojson"))
+for fname in tqdm.tqdm(fnames):
+    with open(fname) as f:
+        category = json.load(f)["features"][0]["properties"]["new_label"]
+        if category in ["unknown", "unlabeled", "human", "natural"]:
+            continue
+        parts = fname.split("/")
+        group = parts[-5]
+        example_id = parts[-4]
+        example_id_to_label[(group, example_id)] = category
+
+split_data = {}
+for group, example_id in example_id_to_label.keys():
+    if group in ["peru3", "peru3_flagged_in_peru", "peru_interesting"]:
+        is_val = hashlib.sha256(example_id.encode()).hexdigest()[0] in ["0", "1", "2", "3", "4", "5", "6", "7"]
+        if is_val:
+            split_data[example_id] = "val"
+        else:
+            split_data[example_id] = "train"
+    elif group in ["nadia2", "nadia3", "brazil_interesting"]:
+        split_data[example_id] = "train"
