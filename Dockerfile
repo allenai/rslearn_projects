d2ccff7b52fe1b003739ee7c0a1ebbc5b9e73526
Favyen Bastani <favyenb@allenai.org>
2024-07-30 12:47:08 -0700

    Add support for launching beaker job to train model

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM pytorch/pytorch:2.4.0-cuda11.8-cudnn9-runtime
+
+RUN apt update
+RUN apt install -y libpq-dev ffmpeg libsm6 libxext6
+COPY rslearn/requirements.txt /opt/rslearn_projects/rslearn/requirements.txt
+RUN pip install -r /opt/rslearn_projects/rslearn/requirements.txt
+COPY requirements.txt /opt/rslearn_projects/requirements.txt
+RUN pip install -r /opt/rslearn_projects/requirements.txt
+ENV PYTHONPATH="${PYTHONPATH}:/opt/rslearn_projects/rslearn:."
+
+COPY /. /opt/rslearn_projects/
+WORKDIR /opt/rslearn_projects
--- rslp/launch_beaker.py
+++ rslp/launch_beaker.py
@@ -1,3 +1,61 @@
-from beaker import Beaker, ExperimentSpec
+import argparse
+import uuid
 
-def launch_job(project, )
+from beaker import Beaker, Constraints, DataMount, DataSource, EnvVar, ExperimentSpec, Priority, TaskResources
+
+DEFAULT_WORKSPACE = "ai2/earth-systems"
+BUDGET = "ai2/prior"
+IMAGE_NAME = "favyen/rslearn"
+
+def launch_job(config_path: str, workspace: str):
+    beaker = Beaker.from_env(default_workspace=workspace)
+
+    with beaker.session():
+        spec = ExperimentSpec.new(
+            budget=BUDGET,
+            description=config_path,
+            beaker_image=IMAGE_NAME,
+            priority=Priority.high,
+            command=["python", "-m", "rslp.main"],
+            arguments=["model", "fit", "--config", config_path],
+            constraints=Constraints(cluster=["ai2/jupiter-cirrascale-2"]),
+            preemptible=True,
+            datasets=[
+                DataMount(
+                    source=DataSource(secret="RSLEARN_GCP_CREDENTIALS"),
+                    mount_path="/etc/credentials/gcp_credentials.json",
+                ),
+            ],
+            env_vars=[
+                EnvVar(
+                    name="WANDB_API_KEY",
+                    secret="RSLEARN_WANDB_API_KEY",
+                ),
+                EnvVar(
+                    name="GOOGLE_APPLICATION_CREDENTIALS",
+                    value="/etc/credentials/gcp_credentials.json",
+                ),
+            ],
+            resources=TaskResources(gpu_count=1),
+        )
+        beaker.experiment.create(config_path.replace("/", "_") + "-" + str(uuid.uuid4()), spec)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Launch beaker experiment for rslearn_projects",
+    )
+    parser.add_argument(
+        "--config_path",
+        type=str,
+        help="Path to configuration file relative to rslearn_projects repository root",
+        required=True,
+    )
+    parser.add_argument(
+        "--workspace",
+        type=str,
+        help="Which workspace to run the experiment in",
+        default=DEFAULT_WORKSPACE,
+    )
+    args = parser.parse_args()
+    launch_job(args.config_path, args.workspace)
