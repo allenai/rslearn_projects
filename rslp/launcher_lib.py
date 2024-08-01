72f778f65ecf7343a5ec95ed9c27a2bce0378034
Favyen Bastani <favyenb@allenai.org>
2024-08-01 10:43:56 -0700

    keep the wandb id so we can avoid making new wandb run when we autoresume

--- rslp/launcher_lib.py
+++ rslp/launcher_lib.py
@@ -1,13 +1,24 @@
+import io
 import os
 import shutil
 import tempfile
+from typing import Optional
 
 from google.cloud import storage
 import yaml
 
 BUCKET_NAME = "rslearn-data"
 CODE_BLOB_PATH = "projects/{project_id}/{experiment_id}/code.zip"
+WANDB_ID_BLOB_PATH = "projects/{project_id}/{experiment_id}/wandb_id"
 
+bucket = None
+
+def get_bucket():
+    global bucket
+    if bucket is None:
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(BUCKET_NAME)
+    return bucket
 
 def get_project_and_experiment(config_path: str) -> tuple[str, str]:
     with open(config_path) as f:
@@ -18,9 +29,7 @@ def get_project_and_experiment(config_path: str) -> tuple[str, str]:
 
 
 def upload_code(project_id: str, experiment_id: str):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(BUCKET_NAME)
-
+    bucket = get_bucket()
     with tempfile.TemporaryDirectory() as tmpdirname:
         print("creating archive of current code state")
         zip_fname = shutil.make_archive(os.path.join(tmpdirname, "archive"), "zip", root_dir=".")
@@ -32,9 +41,7 @@ def upload_code(project_id: str, experiment_id: str):
 
 
 def download_code(project_id: str, experiment_id: str):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(BUCKET_NAME)
-
+    bucket = get_bucket()
     with tempfile.TemporaryDirectory() as tmpdirname:
         print("downloading code acrhive")
         blob_path = CODE_BLOB_PATH.format(project_id=project_id, experiment_id=experiment_id)
@@ -44,3 +51,24 @@ def download_code(project_id: str, experiment_id: str):
         print("extracting archive")
         shutil.unpack_archive(zip_fname, ".", "zip")
         print("extraction complete", flush=True)
+
+
+def upload_wandb_id(project_id: str, experiment_id: str, wandb_id: str):
+    bucket = get_bucket()
+    blob_path = WANDB_ID_BLOB_PATH.format(project_id=project_id, experiment_id=experiment_id)
+    blob = bucket.blob(blob_path)
+    buf = io.BytesIO()
+    buf.write(wandb_id.encode())
+    buf.seek(0)
+    blob.upload_from_file(buf)
+
+
+def download_wandb_id(project_id: str, experiment_id: str) -> Optional[str]:
+    bucket = get_bucket()
+    blob_path = WANDB_ID_BLOB_PATH.format(project_id=project_id, experiment_id=experiment_id)
+    blob = bucket.blob(blob_path)
+    if not blob.exists():
+        return None
+    buf = io.BytesIO()
+    blob.download_to_file(buf)
+    return buf.getvalue().decode()
--- rslp/lightning_cli.py
+++ rslp/lightning_cli.py
@@ -1,9 +1,32 @@
 import fsspec
 import jsonargparse
+import wandb
+from lightning.pytorch.callbacks import Callback
 
 from rslearn.main import RslearnLightningCLI
 
+from rslp import launcher_lib
+
 CHECKPOINT_DIR = "gs://rslearn-data/projects/{project_id}/{experiment_id}/checkpoints/"
+
+
+class SaveWandbRunIdCallback(Callback):
+    """Callback to save the wandb run ID to GCS in case of resume."""
+
+    def __init__(self, project_id: str, experiment_id: str):
+        self.project_id = project_id
+        self.experiment_id = experiment_id
+
+    def on_fit_start(self, trainer, pl_module):
+        """Called just before fit starts I think.
+
+        Args:
+            trainer: the Trainer object.
+            pl_module: the LightningModule object.
+        """
+        run_id = wandb.run.id
+        launcher_lib.upload_wandb_id(self.project_id, self.experiment_id, run_id)
+
 
 class CustomLightningCLI(RslearnLightningCLI):
     """Extended LightningCLI to manage cloud checkpointing and wandb run naming.
@@ -55,13 +78,16 @@ class CustomLightningCLI(RslearnLightningCLI):
 
         # Set the checkpoint directory to canonical GCS location.
         checkpoint_callback = None
+        upload_wandb_callback = None
         if "callbacks" in c.trainer:
             for existing_callback in c.trainer.callbacks:
-                if existing_callback.class_path != "lightning.pytorch.callbacks.ModelCheckpoint":
-                    continue
-                checkpoint_callback = existing_callback
+                if existing_callback.class_path == "lightning.pytorch.callbacks.ModelCheckpoint":
+                    checkpoint_callback = existing_callback
+                if existing_callback.class_path == "SaveWandbRunIdCallback":
+                    upload_wandb_callback = existing_callback
         else:
             c.trainer.callbacks = []
+
         if not checkpoint_callback:
             checkpoint_callback = jsonargparse.Namespace({
                 "class_path": "lightning.pytorch.callbacks.ModelCheckpoint",
@@ -75,6 +101,16 @@ class CustomLightningCLI(RslearnLightningCLI):
         checkpoint_dir = CHECKPOINT_DIR.format(project_id=c.rslp_project, experiment_id=c.rslp_experiment)
         checkpoint_callback.init_args.dirpath = checkpoint_dir
 
+        if not upload_wandb_callback:
+            upload_wandb_callback = jsonargparse.Namespace({
+                "class_path": "SaveWandbRunIdCallback",
+                "init_args": jsonargparse.Namespace({
+                    "project_id": c.rslp_project,
+                    "experiment_id": c.rslp_experiment,
+                }),
+            })
+            c.trainer.callbacks.append(upload_wandb_callback)
+
         # Check if there is an existing checkpoint.
         # If so, and autoresume is disabled, we should throw error.
         # If autoresume is enabled, then we should resume from the checkpoint.
@@ -85,3 +121,8 @@ class CustomLightningCLI(RslearnLightningCLI):
             c.ckpt_path = checkpoint_dir + "last.ckpt"
             print(f"found checkpoint to resume from at {c.ckpt_path}")
             # TODO: additional logic here to continue the wandb run.
+
+            wandb_id = launcher_lib.download_wandb_id(c.rslp_project, c.rslp_experiment)
+            if wandb_id:
+                print(f"resuming wandb run {wandb_id}")
+                c.trainer.logger.init_args.id = wandb_id
