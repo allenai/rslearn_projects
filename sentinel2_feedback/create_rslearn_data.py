3e3b843eaa99ebe7487743a3baef15beb74fda64
Mike Jacobi <mikerjacobi@gmail.com>
2024-09-09 09:21:21 -0700

    able to produce rslearn dataset and run prepare/ingest/materialize

--- sentinel2_feedback/create_rslearn_data.py
+++ sentinel2_feedback/create_rslearn_data.py
@@ -1,15 +1,37 @@
 import argparse
 import csv
+import json
 import math
 import os
 import shutil
 from datetime import datetime, timedelta
+from pathlib import Path
 
+import shapely
 from pydantic import BaseModel
-from pyproj import CRS, Transformer
-from rslearn.dataset import Window
-from rslearn.utils import LocalFileAPI, Projection
-from rslearn.utils.raster_format import GeotiffRasterFormat
+from pyproj import Transformer
+from rslearn.const import WGS84_PROJECTION
+from rslearn.dataset.window import Window
+from rslearn.utils import get_utm_ups_crs
+from rslearn.utils.geometry import Projection, STGeometry
+from upath import UPath
+
+point_geojson = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [[32, 32]],
+            },
+            "properties": {
+                "label": None,
+            },
+        }
+    ],
+    "properties": None,
+}
 
 
 class ArgsModel(BaseModel):
@@ -36,29 +58,42 @@ def latlon_to_utm_zone(lat, lon):
     return epsg_code
 
 
-def create_projection(lat, lon, pixel_size=10):
-    """Creates a Projection object based on the center latitude and longitude."""
-    epsg_code = latlon_to_utm_zone(lat, lon)
-    crs = CRS.from_epsg(epsg_code)
-    return Projection(crs=crs, x_resolution=pixel_size, y_resolution=pixel_size)
-
-
 def calculate_bounds(
-    lat, lon, pixel_width, pixel_height, pixel_size, projection
+    record: Record, projection: Projection
 ) -> tuple[int, int, int, int]:
-    """Calculate the bounds of the image in the projected coordinates."""
-    transformer = Transformer.from_crs("epsg:4326", projection.crs, always_xy=True)
-    center_x, center_y = transformer.transform(lon, lat)
+    pixels_per_tile = 1024
+    point = shapely.Point(record.lon, record.lat)
+    stgeometry = STGeometry(WGS84_PROJECTION, point, None)
+    geometry = stgeometry.to_projection(projection)
+    tile_col = int(geometry.shp.x) // pixels_per_tile
+    tile_row = int(geometry.shp.y) // pixels_per_tile
 
-    half_width = (pixel_width / 2) * pixel_size
-    half_height = (pixel_height / 2) * pixel_size
+    bounds = (
+        tile_col * pixels_per_tile,
+        tile_row * pixels_per_tile,
+        (tile_col + 1) * pixels_per_tile,
+        (tile_row + 1) * pixels_per_tile,
+    )
+    return bounds
 
-    min_x = center_x - half_width
-    max_x = center_x + half_width
-    min_y = center_y - half_height
-    max_y = center_y + half_height
 
-    return (min_x, min_y, max_x, max_y)
+def get_label_data(record: Record, window: Window):
+    return {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [[32, 32]],
+                },
+                "properties": {
+                    "label": record.label,
+                },
+            }
+        ],
+        "properties": window.projection.serialize(),
+    }
 
 
 def create_rslearn_data(args: ArgsModel):
@@ -66,32 +101,21 @@ def create_rslearn_data(args: ArgsModel):
         reader = csv.DictReader(file)
         for row in reader:
             record = Record(**row)
-            projection = create_projection(record.lat, record.lon)
-
-            # Define pixel size and image dimensions (adjust if necessary)
             pixel_size = 10  # 10 meters per pixel for Sentinel-2
-            chip_size = 128  # 128x128 pixel image
-
-            # Calculate the geographic bounds of the PNG image
-            bounds = calculate_bounds(
-                record.lat,
-                record.lon,
-                chip_size,
-                chip_size,
-                pixel_size,
-                projection,
+            crs = get_utm_ups_crs(record.lat, record.lon)
+            projection = Projection(
+                crs=crs, x_resolution=pixel_size, y_resolution=-pixel_size
             )
 
+            bounds = calculate_bounds(record, projection)
             timestamp = datetime.fromisoformat(record.time)
-            window_root = os.path.join(
-                args.out_dir, "windows", record.event_id, record.label
-            )
+            window_root = UPath(f"{args.out_dir}/windows/sentinel2/{record.event_id}")
             os.makedirs(window_root, exist_ok=True)
 
             # Create the Window object
             window = Window(
-                file_api=LocalFileAPI(window_root),
-                group="images",
+                path=window_root,
+                group=record.label,
                 name=record.event_id,
                 projection=projection,
                 bounds=bounds,
@@ -102,30 +126,17 @@ def create_rslearn_data(args: ArgsModel):
             )
             window.save()
 
-            """
-            populate the chip layer
+            # Populate the sentinel2 layer
+            image_layer_dir = os.path.join(window_root, "layers", "sentinel2", "R_G_B")
+            os.makedirs(image_layer_dir, exist_ok=True)
+            Path(f"{image_layer_dir}/completed").touch()
 
-            this works by copying the chip image to the chips directory.
-            """
-            crop = None
-            file_api = window.file_api.get_folder("layers", "chips", "R_G_B")
-            GeotiffRasterFormat().encode_raster(file_api, projection, bounds, crop)
-            dst_chip_path = os.path.join(file_api.to_str(), "chip.png")
-            shutil.copyfile(record.chip_path, dst_chip_path)
-            complete_path = os.path.join(window_root, "layers", "chips", "completed")
-            os.system(f"touch {complete_path}")
-
-            """
-            populate the label layer
-
-            this works by creating a dummy geotiff raster with the same bounds as the chip image
-            and a property to denote the label.
-            """
-            file_api = window.file_api.get_folder("layers", "label", "label")
-            GeotiffRasterFormat().encode_raster(file_api, projection, bounds, crop)
-            # TODO write labels.json with the label
-            complete_path = os.path.join(window_root, "layers", "label", "completed")
-            os.system(f"touch {complete_path}")
+            # Populate the label layer
+            label_layer_dir = os.path.join(window_root, "layers", "label")
+            os.makedirs(label_layer_dir, exist_ok=True)
+            with open(os.path.join(label_layer_dir, "data.geojson"), "w") as f:
+                json.dump(get_label_data(record, window), f)
+            Path(f"{label_layer_dir}/completed").touch()
 
 
 if __name__ == "__main__":
@@ -147,5 +158,9 @@ if __name__ == "__main__":
     parsed_args = parser.parse_args()
     args = ArgsModel(**vars(parsed_args))  # convert parsed args to pydantic model
     os.makedirs(args.out_dir, exist_ok=True)
+
+    # Copy the model architecture definition to the window directory
     shutil.copyfile("config.json", os.path.join(args.out_dir, "config.json"))
+    shutil.copyfile("model_config.yaml", os.path.join(args.out_dir, "config.yaml"))
+
     create_rslearn_data(args)
