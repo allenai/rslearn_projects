8abc684974c321148fa0fb94a5f20ed4c1827eb3
yawenzzzz <yawenz@allenai.org>
2024-09-16 18:26:10 +0000

    merge conflicts in rebase

--- rslp/launch_beaker.py
+++ rslp/launch_beaker.py
@@ -20,7 +20,7 @@ from rslp import launcher_lib
 
 DEFAULT_WORKSPACE = "ai2/earth-systems"
 BUDGET = "ai2/prior"
-IMAGE_NAME = "favyen/rslearn"
+IMAGE_NAME = "yawenzzzz/rslearn_sam2"
 
 
 def launch_job(
