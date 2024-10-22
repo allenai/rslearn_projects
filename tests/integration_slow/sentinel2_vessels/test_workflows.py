a7b5de523de8dee34b023574cfb7e0fc1ee78a2d
Favyen Bastani <favyenb@allenai.org>
2024-10-22 11:15:35 -0700

    fix lint issues and stuff

--- tests/integration_slow/sentinel2_vessels/test_workflows.py
+++ tests/integration_slow/sentinel2_vessels/test_workflows.py
@@ -4,7 +4,7 @@ import pathlib
 from rslp.sentinel2_vessels import predict_pipeline
 
 
-def test_predict_pipeline(tmp_path: pathlib.Path):
+def test_predict_pipeline(tmp_path: pathlib.Path) -> None:
     scratch_path = tmp_path / "scratch"
     scratch_path.mkdir(parents=True, exist_ok=True)
     crop_path = tmp_path / "crops"
@@ -14,6 +14,6 @@ def test_predict_pipeline(tmp_path: pathlib.Path):
         scene_id="S2B_MSIL1C_20200206T222749_N0209_R072_T01LAL_20200206T234349",
         scratch_path=str(scratch_path),
         crop_path=str(crop_path),
-        json_path=json_path,
+        json_path=str(json_path),
     )
     assert os.path.exists(json_path)
