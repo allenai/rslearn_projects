4fb0a690bfa0950c05930a9f6de5ea208b5bd7a6
Favyen Bastani <favyenb@allenai.org>
2024-11-11 14:44:05 -0800

    gcp rtree index not working after august 2024 ...

--- rslp/utils/rslearn.py
+++ rslp/utils/rslearn.py
@@ -15,7 +15,13 @@ from rslp.lightning_cli import CustomLightningCLI
 
 
 def materialize_dataset(
-    ds_path: UPath, group: str | None = None, workers: int = 32
+    ds_path: UPath,
+    group: str | None = None,
+    workers: int = 32,
+    initial_prepare_job: bool = False,
+    prepare_workers: int | None = None,
+    ingest_workers: int | None = None,
+    materialize_workers: int | None = None,
 ) -> None:
     """Materialize the specified dataset by running prepare/ingest/materialize.
 
@@ -23,25 +29,42 @@ def materialize_dataset(
         ds_path: the dataset root.
         group: limit dataset actions to this group.
         workers: number of workers to use.
+        initial_prepare_job: set True if initial job during prepare is needed, e.g. if
+            the data source creates an index first.
+        prepare_workers: use this many workers for prepare stage (overrides workers
+            argument)
+        ingest_workers: use this many workers for ingest stage (overrides workers
+            argument)
+        materialize_workers: use this many workers for materialize stage (overrides
+            workers argument)
     """
     dataset = Dataset(ds_path)
+
+    if prepare_workers is None:
+        prepare_workers = workers
+    if ingest_workers is None:
+        ingest_workers = workers
+    if materialize_workers is None:
+        materialize_workers = workers
+
     apply_on_windows(
         PrepareHandler(force=False),
         dataset,
-        workers=workers,
+        workers=prepare_workers,
         group=group,
+        use_initial_job=initial_prepare_job,
     )
     apply_on_windows(
         IngestHandler(),
         dataset,
-        workers=workers,
+        workers=ingest_workers,
         group=group,
         use_initial_job=False,
     )
     apply_on_windows(
         MaterializeHandler(),
         dataset,
-        workers=workers,
+        workers=materialize_workers,
         group=group,
         use_initial_job=False,
     )
