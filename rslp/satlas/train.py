f916d5cc977e39ec5fdaf4f08d0a7e443df2707b
Favyen Bastani <favyenb@allenai.org>
2025-01-09 08:29:38 -0800

    upgrade solar farm ingestion

--- rslp/satlas/train.py
+++ rslp/satlas/train.py
@@ -67,8 +67,30 @@ class TeePipe(torch.nn.Module):
                 example, if the input is ABCDEF and first encoder should see ABC while
                 second should see DEF, then the list should be [[0, 1, 2], [3, 4, 5]].
         """
-        self.encoders = encoders
+        super().__init__()
+        self.encoders = torch.nn.ModuleList(encoders)
         self.channels = channels
+
+    def get_backbone_channels(self) -> list:
+        """Returns the output channels of this model when used as a backbone.
+
+        Returns:
+            the output channels of the backbone as a list of (downsample_factor, depth)
+            tuples.
+        """
+        # We assume that each encoder outputs features at matching resolutions.
+        out_channels = self.encoders[0].get_backbone_channels()
+
+        for encoder in self.encoders[1:]:
+            cur_channels = encoder.get_backbone_channels()
+            for idx, (downsample_factor, depth) in enumerate(cur_channels):
+                if out_channels[idx][0] != downsample_factor:
+                    raise ValueError(
+                        "encoders have mis-matching resolutions of output feature maps"
+                    )
+                out_channels[idx][1] += depth
+
+        return out_channels
 
     def forward(self, inputs: list[dict[str, Any]]) -> list[torch.Tensor]:
         """Compute features.
@@ -78,17 +100,16 @@ class TeePipe(torch.nn.Module):
                 process.
         """
         # index in feature map -> encoder index -> feature map
-        all_features: list[list[torch.Tensor]] | None = None
+        all_features: list[list[torch.Tensor]] = [
+            [] for _ in self.get_backbone_channels()
+        ]
 
         for encoder, cur_channels in zip(self.encoders, self.channels):
             cur_features = encoder(
                 [{"image": inp["image"][cur_channels, :, :]} for inp in inputs]
             )
-            if all_features is None:
-                all_features = [[] for _ in cur_features]
             for idx, feat_map in enumerate(cur_features):
                 all_features[idx].append(feat_map)
 
         # Final feature map should concatenate at each scale.
-        assert all_features is not None
         return [torch.cat(feat_map_list, dim=1) for feat_map_list in all_features]
