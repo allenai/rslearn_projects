98f99a0d4b017310c2f9a8596e75d2eeae6545ba
Favyen Bastani <favyenb@allenai.org>
2025-02-05 13:15:49 -0800

    beaker

--- rslp/common/__init__.py
+++ rslp/common/__init__.py
@@ -1,8 +1,10 @@
 """Pipelines common across projects."""
 
+from .beaker_launcher import launch_job
 from .worker import launch_workers, worker_pipeline
 
 workflows = {
     "worker": worker_pipeline,
     "launch": launch_workers,
+    "beaker_launcher": launch_job,
 }
