0a11587fe35bf3fb6707927b9dfef653020e4f07
Favyen Bastani <favyenb@allenai.org>
2025-02-18 11:44:16 -0800

    Fix the copy step

--- rslp/forest_loss_driver/copy.py
+++ rslp/forest_loss_driver/copy.py
@@ -8,13 +8,16 @@ It may not be needed in other workflows.
 """
 
 import multiprocessing
+from typing import Any
 
 import tqdm
-from rslearn.dataset import Dataset
+from rslearn.dataset import Dataset, Window
 from rslearn.utils.mp import star_imap_unordered
 from upath import UPath
 
 from rslp.utils.fs import copy_file
+
+from .const import WINDOWS_FNAME
 
 NEEDED_LAYERS = [
     "best_pre_0",
@@ -48,6 +51,56 @@ def get_relative_suffix(base_dir: UPath, fname: UPath) -> str:
     return suffix
 
 
+def get_copy_jobs_for_window(
+    window: Window, src_path: UPath, dst_path: UPath
+) -> list[dict[str, Any]]:
+    """Get the job dictionaries for copying all the needed files for this window.
+
+    Args:
+        window: the window to copy from.
+        src_path: the source base directory containing the window.
+        dst_path: the destination base directory to copy to.
+
+    Returns:
+        list of dictionaries with src_fname and dst_fname keys suitable to pass to
+            copy_file function via star_imap.
+    """
+    copy_file_jobs = []
+
+    # Copy files in the needed layers.
+    for layer_name in NEEDED_LAYERS:
+        layer_dir = window.get_layer_dir(layer_name)
+        for src_fname in layer_dir.rglob("*"):
+            # Only copy certain files.
+            # This also ensures directories are skipped.
+            if src_fname.name != "completed" and src_fname.name.split(".")[-1] not in [
+                "png",
+                "geojson",
+            ]:
+                continue
+
+            dst_fname = dst_path / get_relative_suffix(src_path, src_fname)
+            copy_file_jobs.append(
+                dict(
+                    src_fname=src_fname,
+                    dst_fname=dst_fname,
+                )
+            )
+
+    # Copy additional JSON files.
+    for window_suffix in ["info.json", "metadata.json", "least_cloudy_times.json"]:
+        src_fname = window.path / window_suffix
+        dst_fname = dst_path / get_relative_suffix(src_path, src_fname)
+        copy_file_jobs.append(
+            dict(
+                src_fname=src_fname,
+                dst_fname=dst_fname,
+            )
+        )
+
+    return copy_file_jobs
+
+
 def copy_dataset(src_path: UPath, dst_path: UPath, workers: int = 32) -> None:
     """Copy the rslearn forest loss driver dataset.
 
@@ -63,38 +116,31 @@ def copy_dataset(src_path: UPath, dst_path: UPath, workers: int = 32) -> None:
     # Copy dataset configuration.
     copy_file(src_path / "config.json", dst_path / "config.json")
 
-    # Copy per-window files. We create argument list for copy_file and then run it in
-    # parallel.
+    # Copy index of windows with outputs.
+    copy_file(src_path / WINDOWS_FNAME, dst_path / WINDOWS_FNAME)
+
+    # Copy per-window files. We first list the windows, and then in parallel we check
+    # each window to enumerate the files that need to be copied.
     dataset = Dataset(src_path)
     windows = dataset.load_windows(workers=workers)
-    copy_file_jobs = []
-    for window in windows:
-        # Copy files in the needed layers.
-        for layer_name in NEEDED_LAYERS:
-            layer_dir = window.get_layer_dir(layer_name)
-            for _, _, fnames in layer_dir.walk():
-                for src_fname in fnames:
-                    dst_fname = dst_path / get_relative_suffix(src_path, src_fname)
-                    copy_file_jobs.append(
-                        dict(
-                            src_fname=src_fname,
-                            dst_fname=dst_fname,
-                        )
-                    )
-
-        # Copy info.json and metadata.json.
-        for window_suffix in ["info.json", "metadata.json"]:
-            src_fname = window.path / window_suffix
-            dst_fname = dst_path / get_relative_suffix(src_path, src_fname)
-            copy_file_jobs.append(
-                dict(
-                    src_fname=src_fname,
-                    dst_fname=dst_fname,
-                )
-            )
-
     p = multiprocessing.Pool(workers)
+    list_jobs = [
+        dict(
+            window=window,
+            src_path=src_path,
+            dst_path=dst_path,
+        )
+        for window in windows
+    ]
+    outputs = star_imap_unordered(p, get_copy_jobs_for_window, list_jobs)
+    copy_file_jobs = []
+    for window_copy_file_jobs in tqdm.tqdm(
+        outputs, total=len(list_jobs), desc="Listing source files"
+    ):
+        copy_file_jobs.extend(window_copy_file_jobs)
+
+    # Now perform the actual copies, also in parallel.
     outputs = star_imap_unordered(p, copy_file, copy_file_jobs)
-    for _ in tqdm.tqdm(outputs, total=len(copy_file_jobs)):
+    for _ in tqdm.tqdm(outputs, total=len(copy_file_jobs), desc="Copying files"):
         pass
     p.close()
