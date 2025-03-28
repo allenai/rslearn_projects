fix normalization

rslp/helios/model.py
@@ -103,7 +103,9 @@ class Helios(torch.nn.Module):
         kwargs["timestamps"] = timestamps
 
         for k, v in kwargs.items():
-            print("INPUT", k, v.shape)
+            print(
+                "INPUT", k, v.shape, v.min(), v.max(), v.float().mean(), v.float().std()
+            )
 
         sample = MaskedHeliosSample(**kwargs)
 
@@ -124,7 +126,10 @@ class Helios(torch.nn.Module):
         # Pool over the modalities, so we get one BCHW feature map.
         pooled = torch.stack(features, dim=0).mean(dim=0)
         if pooled.shape[0] >= 2:
-            print(pooled.shape, "first", pooled[0, 16:20, 16:20, 0:3])
-            print(pooled.shape, "second", pooled[1, 16:20, 16:20, 0:3])
+            print(pooled.shape, "first", pooled[0, 4:8, 4:8, 0:3])
+            print(pooled.shape, "second", pooled[1, 4:8, 4:8, 0:3])
+
+        print("feat1", self.model.blocks[1].mlp.fc1.weight.flatten()[0:3])
+        print("feat2", self.model.blocks[2].attn.q.weight.flatten()[0:3])
 
         return [pooled]