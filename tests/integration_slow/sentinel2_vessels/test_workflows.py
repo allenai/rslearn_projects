9655c4ae1891213b6b6a402b85b838014264ba01
Favyen Bastani <favyenb@allenai.org>
2025-04-07 13:44:27 -0700

    Fix issue with image_files argument in sentinel2_vessels pipeline

--- tests/integration_slow/sentinel2_vessels/test_workflows.py
+++ tests/integration_slow/sentinel2_vessels/test_workflows.py
@@ -3,10 +3,15 @@ import pathlib
 
 from upath import UPath
 
-from rslp.sentinel2_vessels.predict_pipeline import PredictionTask, predict_pipeline
+from rslp.sentinel2_vessels.predict_pipeline import (
+    ImageFile,
+    PredictionTask,
+    predict_pipeline,
+)
 
 
-def test_predict_pipeline(tmp_path: pathlib.Path) -> None:
+def test_predict_pipeline_scene_with_vessels(tmp_path: pathlib.Path) -> None:
+    """Verify that prediction detects vessels in a scene that has lots of vessels."""
     should_have_vessels_task = PredictionTask(
         scene_id="S2A_MSIL1C_20161130T110422_N0204_R094_T30UYD_20161130T110418",
         json_path=str(tmp_path / "1.json"),
@@ -31,3 +36,45 @@ def test_predict_pipeline(tmp_path: pathlib.Path) -> None:
         # This is some scene off coast of UK which should have a bunch of vessels.
         vessels = json.load(f)
         assert len(vessels) > 0
+
+
+def test_predict_pipeline_image_files(tmp_path: pathlib.Path) -> None:
+    """Verify that prediction works when we pass individual image files."""
+    # Reference the images on GCS.
+    gcs_fname_template = "gs://gcp-public-data-sentinel-2/tiles/30/U/YD/S2A_MSIL1C_20180904T110621_N0206_R137_T30UYD_20180904T133425.SAFE/GRANULE/L1C_T30UYD_A016722_20180904T110820/IMG_DATA/T30UYD_20180904T110621_{band}.jp2"
+    image_files = []
+    for band in [
+        "B01",
+        "B02",
+        "B03",
+        "B04",
+        "B05",
+        "B06",
+        "B07",
+        "B08",
+        "B09",
+        "B10",
+        "B11",
+        "B12",
+        "B8A",
+    ]:
+        image_files.append(
+            ImageFile(
+                bands=[band],
+                fname=gcs_fname_template.format(band=band),
+            )
+        )
+
+    tasks = [
+        PredictionTask(
+            image_files=image_files,
+        )
+    ]
+
+    scratch_path = tmp_path / "scratch"
+    scratch_path.mkdir(parents=True, exist_ok=True)
+
+    predict_pipeline(
+        tasks=tasks,
+        scratch_path=str(scratch_path),
+    )
