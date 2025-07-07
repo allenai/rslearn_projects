b08c9c531fb57ba535ad97dfa3f718b8b614656e
Favyen Bastani <favyenb@allenai.org>
2025-07-07 11:07:40 -0700

    Update script to allow reporting metric for multiple runs using regex

--- rslp/helios/scripts/get_best_wandb_metric.py
+++ rslp/helios/scripts/get_best_wandb_metric.py
@@ -5,6 +5,7 @@ set correctly.
 """
 
 import argparse
+import re
 
 import wandb
 
@@ -14,30 +15,62 @@ DEFAULT_WANDB_ENTITY = "eai-ai2"
 def get_best_metric(
     entity_name: str,
     project_name: str,
-    run_id: str,
     metric_name: str,
     mode: str = "max",
-) -> float:
+    run_id: str | None = None,
+    run_regex: str | None = None,
+    runs_after: str | None = None,
+) -> dict[str, float]:
     """Get the best value of the metric over the history of the run.
 
     Args:
         entity_name: the W&B entity containing the run.
         project_name: the W&B project containing the run.
-        run_id: the W&B run ID.
         metric_name: the name of the metric to get best value of. It must correspond to
             a supported operation on pandas DataFrame.
         mode: how to aggregate the metric values over the history. It must be a
             supported operation on pandas dataframe.
+        run_id: the W&B run ID. Exactly one of run_id and run_regex must be specified.
+        run_regex: a regex for matching the W&B run name.
+        runs_after: only consider runs after this time.
 
     Returns:
-        the best value of the metric.
+        a map from run name to the best value of the metric for that run.
     """
+    if (run_id is None and run_regex is None) or (
+        run_id is not None and run_regex is not None
+    ):
+        raise ValueError("exactly one of run_id and run_regex must be specified")
+
     api = wandb.Api()
-    run = api.run(f"{entity_name}/{project_name}/{run_id}")
-    history_df = run.history()
-    op = getattr(history_df[metric_name], mode)
-    best_value = op()
-    return best_value
+    runs = []
+
+    if run_id is not None:
+        run = api.run(f"{entity_name}/{project_name}/{run_id}")
+        runs.append(run)
+
+    elif run_regex is not None:
+        pattern = re.compile(run_regex)
+        for run in api.runs(f"{entity_name}/{project_name}"):
+            if not pattern.match(run.name):
+                continue
+            # We can just use string comparison for the date, which is of the form:
+            # "2025-06-27T21:49:03.775779Z"
+            if run.metadata["startedAt"] < runs_after:
+                continue
+            runs.append(run)
+
+    best_values = {}
+    for run in runs:
+        print(f"getting metric for {run.name}")
+        history_df = run.history()
+        if metric_name not in history_df:
+            print(f"warning: run {run.name} does not have metric {metric_name}")
+            continue
+        op = getattr(history_df[metric_name], mode)
+        best_values[run.name] = op()
+
+    return best_values
 
 
 if __name__ == "__main__":
@@ -58,8 +91,14 @@ if __name__ == "__main__":
     parser.add_argument(
         "--run",
         type=str,
-        required=True,
         help="W&B run ID",
+        default=None,
+    )
+    parser.add_argument(
+        "--run_regex",
+        type=str,
+        help="Regex for matching W&B run name",
+        default=None,
     )
     parser.add_argument(
         "--metric",
@@ -74,9 +113,22 @@ if __name__ == "__main__":
         help="Mode to determine what metric value is best (one of max or min)",
         default="max",
     )
+    parser.add_argument(
+        "--runs_after",
+        type=str,
+        help="Only process runs after this date",
+        default=None,
+    )
     args = parser.parse_args()
 
-    best_value = get_best_metric(
-        args.entity, args.project, args.run, args.metric, args.mode
+    best_values = get_best_metric(
+        args.entity,
+        args.project,
+        metric_name=args.metric,
+        mode=args.mode,
+        run_regex=args.run_regex,
+        run_id=args.run,
+        runs_after=args.runs_after,
     )
-    print(best_value)
+    for run_name in sorted(best_values.keys()):
+        print(run_name, best_values[run_name])
