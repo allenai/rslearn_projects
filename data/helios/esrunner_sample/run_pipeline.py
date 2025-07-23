617427e9b483c844fb30083c15e10e8fa6a064f8
Josh Hansen <joshuah@allenai.org>
2025-07-23 08:51:55 -0700

    Add harness example

---
+++ data/helios/esrunner_sample/run_pipeline.py
@@ -0,0 +1,42 @@
+import logging
+from pathlib import Path
+
+from rslp.espredict_runner import EsPredictRunner
+
+CONFIG_PATH = Path(__file__).parent
+MODEL_CONFIG_PATH = CONFIG_PATH / 'model.yaml'
+DATASET_CONFIG_PATH = CONFIG_PATH / 'dataset.json'
+PARTITION_PATH = CONFIG_PATH / 'partition_strategies.yaml'
+POSTPROCESS_PATH = CONFIG_PATH / 'postprocessing_strategies.yaml'
+PREDICTION_REQUEST_PATH = CONFIG_PATH / 'prediction_requests/test-request1.geojson'
+
+logging.basicConfig(level=logging.INFO)
+
+
+def main():
+    runner = EsPredictRunner(
+        MODEL_CONFIG_PATH,
+        DATASET_CONFIG_PATH,
+        PARTITION_PATH,
+        POSTPROCESS_PATH,
+        PREDICTION_REQUEST_PATH,
+        scratch_path=CONFIG_PATH / 'scratch'
+    )
+    partitions = runner.partition()
+    for partition_id in partitions:
+        runner.build_dataset(partition_id)
+        runner.run_inference(partition_id)
+        runner.postprocess(partition_id)
+
+    runner.combine(partitions)
+
+
+# runner = EsFineTuneRunner(.....)
+# runner.create_windows()
+# runner.build_dataset()
+# runner.train()
+# runner.evaluate()
+
+
+if __name__ == "__main__":
+    main()
