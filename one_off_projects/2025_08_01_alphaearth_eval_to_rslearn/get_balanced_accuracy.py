a1d73c51fec4477cf3a67ba196823c3443351503
Favyen Bastani <favyenb@allenai.org>
2025-08-07 14:06:26 -0700

    add gse embeddings and misc options for computing accuracy

--- one_off_projects/2025_08_01_alphaearth_eval_to_rslearn/get_balanced_accuracy.py
+++ one_off_projects/2025_08_01_alphaearth_eval_to_rslearn/get_balanced_accuracy.py
@@ -1,13 +1,67 @@
 import argparse
 import json
 import random
+import multiprocessing
 
 import numpy as np
+import rasterio
+import tqdm
 import torch
-from rslearn.dataset import Dataset
+from rslearn.const import WGS84_PROJECTION
+from rslearn.dataset import Dataset, Window
+from rslearn.utils.raster_format import GeotiffRasterFormat
 from sklearn.metrics import balanced_accuracy_score
 from torch import nn
 from upath import UPath
+
+
+def load_embedding(embedding_fname: UPath) -> torch.Tensor | None:
+    if not embedding_fname.exists():
+        print(f"warning: no embedding at {embedding_fname}")
+        return None
+    with embedding_fname.open("rb") as f:
+        return torch.as_tensor(np.load(f))
+
+
+def load_gse_embedding(window: Window) -> torch.Tensor | None:
+    # Search through the layer directories for a GeoTIFF that is not 0s.
+    for fname in window.path.glob("layers/gse*/*/geotiff.tif"):
+        with rasterio.open(fname.path) as raster:
+            array = raster.read()
+        if array.max() == 0:
+            continue
+        return torch.tensor(array[:, array.shape[1]//2, array.shape[2]//2]) / 8192 - 1
+    return None
+
+
+def load_location_embedding(window: Window) -> torch.Tensor:
+    wgs84_geom = window.get_geometry().to_projection(WGS84_PROJECTION)
+    lon = wgs84_geom.shp.centroid.x
+    lat = wgs84_geom.shp.centroid.y
+    return torch.tensor([lon, lat])
+
+
+def load_pixel_embedding(window: Window) -> torch.Tensor:
+    """Get Sentinel-2 pixel values embedding.
+
+    We load center 4x4, averaging on all bands but concatenating over time.
+    """
+    for group_idx in range(12):
+        if not window.is_layer_completed("sentinel2", group_idx):
+            return None
+    values = []
+    for group_idx in range(12):
+        cur_values = []
+        for bands in [["B01", "B09", "B10"], ["B02", "B03", "B04", "B08"], ["B05", "B06", "B07", "B8A", "B11", "B12"]]:
+            raster_dir = window.get_raster_dir("sentinel2", bands, group_idx)
+            array = GeotiffRasterFormat().decode_raster(raster_dir, window.projection, window.bounds)
+            center_col = array.shape[2] // 2
+            center_row = array.shape[1] // 2
+            center_crop = array[:, center_row-2:center_row+2, center_col-2:center_col+2]
+            mean_values = center_crop.mean(axis=(1, 2))
+            cur_values.extend(mean_values.tolist())
+        values.append(torch.tensor(cur_values))
+    return torch.cat(values, dim=0)
 
 
 def run_knn_for_k(
@@ -16,6 +70,7 @@ def run_knn_for_k(
     test_embeddings: torch.Tensor,
     num_classes: int,
     k: int,
+    sim_mode: str,
 ) -> torch.Tensor:
     device = torch.device("cuda")
     train_embeddings = train_embeddings.to(device=device)
@@ -29,22 +84,27 @@ def run_knn_for_k(
         test_embedding = (
             test_embeddings[idx].unsqueeze(dim=0).repeat(train_embeddings.shape[0], 1)
         )
-        sims = cos(test_embedding, train_embeddings)
+
+        if sim_mode == "cos":
+            sims = cos(test_embedding, train_embeddings)
+        elif sim_mode == "l2":
+            sims = -torch.square(test_embedding - train_embeddings).sum(dim=-1)
+
         top_k = torch.topk(sims, k=k)
-        top_k_values = top_k.values
         top_k_indices = top_k.indices
 
         fetched_labels = train_labels[top_k_indices]
         fetched_onehots = nn.functional.one_hot(fetched_labels, num_classes=num_classes)
-        distances = top_k_values.clone().div_(0.07).exp_()
-        weighted_sum_onehots = (distances.unsqueeze(dim=1) * fetched_onehots).sum(dim=0)
-        prediction = torch.argmax(weighted_sum_onehots)
+        sum_onehots = fetched_onehots.sum(dim=0)
+        prediction = torch.argmax(sum_onehots)
         all_preds.append(prediction)
 
     return torch.LongTensor(all_preds).cpu()
 
 
 if __name__ == "__main__":
+    multiprocessing.set_start_method("forkserver")
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--ds_path",
@@ -61,7 +121,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--samples",
         type=int,
-        help="Number of samples",
+        help="Number of samples per class or partition",
         required=True,
     )
     parser.add_argument(
@@ -71,59 +131,100 @@ if __name__ == "__main__":
         required=True,
     )
     parser.add_argument(
-        "--classes",
+        "--embed_fname",
         type=str,
-        help="Comma-separated list of classes",
+        help="Filename to use to save the embeddings, or 'gse' to load google satellite embedding from layer dir, or 'loc' to use location",
         required=True,
     )
     parser.add_argument(
-        "--embed_fname",
+        "--workers",
+        type=int,
+        help="Number of worker processes",
+        default=32,
+    )
+    parser.add_argument(
+        "--sim_mode",
         type=str,
-        help="Filename to use to save the embeddings",
-        required=True,
+        help="Similarity mode, cos or l2",
+        default="l2",
     )
     args = parser.parse_args()
-    class_names = args.classes.split(",")
 
-    # Get all train and test embeddings.
+    # Load windows and all embeddings.
     dataset = Dataset(UPath(args.ds_path))
+    windows = dataset.load_windows(workers=args.workers)
+    p = multiprocessing.Pool(args.workers)
+    if args.embed_fname == "gse":
+        embeddings = list(tqdm.tqdm(p.imap(load_gse_embedding, windows), desc="Loading embeddings", total=len(windows)))
+    elif args.embed_fname == "loc":
+        embeddings = list(tqdm.tqdm(p.imap(load_location_embedding, windows), desc="Loading embeddings", total=len(windows)))
+    elif args.embed_fname == "pixel":
+        embeddings = list(tqdm.tqdm(p.imap(load_pixel_embedding, windows), desc="Loading embeddings", total=len(windows)))
+    else:
+        embeddings = list(tqdm.tqdm(p.imap(load_embedding, [window.path / args.embed_fname for window in windows], chunksize=1), desc="Loading embeddings", total=len(windows)))
+    p.close()
+
+    # Get train and test embeddings, along with label / partition.
+    class_names = []
     train_embedding_list = []
     train_labels = []
+    train_partitions = []
     test_embedding_list = []
     test_labels = []
-    for window in dataset.load_windows(workers=32):
-        embedding_fname = window.path / args.embed_fname
-        if not embedding_fname.exists():
-            print(f"skipping embeddings for window {window.group}/{window.name} since {embedding_fname} does not exist")
+    for window, embedding in zip(windows, embeddings):
+        if embedding is None:
             continue
-        with embedding_fname.open("rb") as f:
-            embedding = torch.as_tensor(np.load(f))
-        label_fname = window.path / "layers" / "label" / "data.geojson"
-        with label_fname.open("r") as f:
-            fc = json.load(f)
-        class_name = fc["features"][0]["properties"]["label"]
+
+        class_name = window.options["label"]
+        if class_name not in class_names:
+            class_names.append(class_name)
         label = class_names.index(class_name)
 
         if window.group == "train":
             train_embedding_list.append(embedding)
             train_labels.append(label)
+
+            # Get partition, or for classification tasks we use the label instead.
+            if window.options["partition"] is not None:
+                train_partitions.append(window.options["partition"])
+            else:
+                train_partitions.append(label)
+
         elif window.group == "test":
             test_embedding_list.append(embedding)
             test_labels.append(label)
+
         else:
             raise ValueError(f"expected all windows to be in train or test but got group {window.group}")
+
     test_embeddings = torch.stack(test_embedding_list, dim=0)
     test_labels = torch.tensor(test_labels, dtype=torch.int64)
+
+    print(f"got n_train={len(train_embedding_list)}, n_test={len(test_embedding_list)}, classes={class_names}, embed_size={len(test_embeddings[0])}")
+
+    train_indexes_by_partition = {}
+    for train_idx, partition in enumerate(train_partitions):
+        if partition not in train_indexes_by_partition:
+            train_indexes_by_partition[partition] = []
+        train_indexes_by_partition[partition].append(train_idx)
+
+    print({partition: len(partition_indexes) for partition, partition_indexes in train_indexes_by_partition.items()})
 
     accuracy_scores = []
     for fold_idx in range(args.repeats):
         print(f"evaluating for fold {fold_idx}")
-        # Take subset of train to use.
-        cur_train_indices = random.sample(list(range(len(train_embedding_list))), args.samples)
+        # Take a balanced subset of the training data.
+        cur_train_indices = []
+        for partition_indexes in train_indexes_by_partition.values():
+            if args.samples == 0:
+                cur_train_indices.extend(partition_indexes)
+            else:
+                cur_train_indices.extend(random.sample(partition_indexes, args.samples))
+
         cur_train_embeddings = torch.stack([train_embedding_list[idx] for idx in cur_train_indices], dim=0)
         cur_train_labels = torch.tensor([train_labels[idx] for idx in cur_train_indices], dtype=torch.int64)
 
-        preds = run_knn_for_k(cur_train_embeddings, cur_train_labels, test_embeddings, len(class_names), args.k)
+        preds = run_knn_for_k(cur_train_embeddings, cur_train_labels, test_embeddings, len(class_names), args.k, args.sim_mode)
         accuracy_score = balanced_accuracy_score(test_labels, preds)
         accuracy_scores.append(accuracy_score)
 
