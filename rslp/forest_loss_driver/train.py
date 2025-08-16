db75d7c44c3d1f86976a56d9226d0df778d8d619
Favyen Bastani <fbastani@perennate.com>
2025-08-15 22:27:16 -0400

    update forest loss driver lm for new api

--- rslp/forest_loss_driver/train.py
+++ rslp/forest_loss_driver/train.py
@@ -1,12 +1,10 @@
 """Remaps categories for Amazon Conservation task to consolidated category set."""
 
-import os
 from typing import Any
 
 import numpy as np
 import torch
 import wandb
-from PIL import Image
 from rslearn.train.lightning_module import RslearnLightningModule
 from rslearn.train.tasks.classification import ClassificationTask
 from rslearn.utils import Feature
@@ -97,103 +95,25 @@ class ForestLossTask(ClassificationTask):
 class ForestLossLightningModule(RslearnLightningModule):
     """Lightning module extended with val / test confusion matrix reporting."""
 
-    def training_step(
-        self, batch: Any, batch_idx: int, dataloader_idx: int = 0
-    ) -> torch.Tensor:
-        """Customized training step.
-
-        Args:
-            batch: the batch contents.
-            batch_idx: the batch index.
-            dataloader_idx: the index of the dataloader creating this batch.
-        """
-        # Copied over from RslearnLightningModule.training_step.
-        inputs, targets, _ = batch
-        batch_size = len(inputs)
-        _, loss_dict = self(inputs, targets)
-        train_loss = sum(loss_dict.values())
-        self.log_dict(
-            {"train_" + k: v for k, v in loss_dict.items()},
-            batch_size=batch_size,
-            prog_bar=True,
-            on_step=False,
-            on_epoch=True,
-        )
-        self.log(
-            "train_loss",
-            train_loss,
-            batch_size=batch_size,
-            on_step=False,
-            on_epoch=True,
-        )
-
-        # Temporary stuff to visualize the inputs.
-        if False:
-            for image_idx, inp in enumerate(inputs):
-                target = targets[image_idx]["class"]
-                if not target["valid"]:
-                    continue
-                category = CATEGORIES[target["class"]]
-                pre_im = np.clip(
-                    inp["image"][0:3, :, :].permute(1, 2, 0).detach().cpu().numpy()
-                    * 255,
-                    0,
-                    255,
-                ).astype(np.uint8)
-                Image.fromarray(pre_im).save(
-                    f"/home/favyenb/vis/vis/inp_{batch_idx}_{image_idx}_{category}_pre.png"
-                )
-                post_im = np.clip(
-                    inp["image"][9:12, :, :].permute(1, 2, 0).detach().cpu().numpy()
-                    * 255,
-                    0,
-                    255,
-                ).astype(np.uint8)
-                Image.fromarray(post_im).save(
-                    f"/home/favyenb/vis/vis/inp_{batch_idx}_{image_idx}_{category}_post.png"
-                )
-
-        return train_loss
-
     def on_validation_epoch_start(self) -> None:
         """Initialize val confusion matrix."""
         self.probs: list = []
         self.y_true: list = []
 
-    def validation_step(
-        self, batch: Any, batch_idx: int, dataloader_idx: int = 0
+    def on_val_forward(
+        self,
+        inputs: list[dict[str, Any]],
+        targets: list[dict[str, Any]],
+        model_outputs: dict[str, Any],
     ) -> None:
-        """Compute val performance and also record for confusion matrix.
+        """Hook to run after the forward pass of the model during validation.
 
         Args:
-            batch: the batch contents.
-            batch_idx: the batch index.
-            dataloader_idx: the index of the dataloader creating this batch.
+            inputs: The input batch.
+            targets: The target batch.
+            model_outputs: The output of the model, with keys "outputs" and "loss_dict", and possibly other keys.
         """
-        # Code below is copied from RslearnLightningModule.validation_step.
-        inputs, targets, _ = batch
-        batch_size = len(inputs)
-        outputs, loss_dict = self(inputs, targets)
-        val_loss = sum(loss_dict.values())
-        self.log_dict(
-            {"val_" + k: v for k, v in loss_dict.items()},
-            batch_size=batch_size,
-            on_step=False,
-            on_epoch=True,
-        )
-        self.log(
-            "val_loss",
-            val_loss,
-            batch_size=batch_size,
-            prog_bar=True,
-            on_step=False,
-            on_epoch=True,
-        )
-        self.val_metrics.update(outputs, targets)
-        self.log_dict(self.val_metrics, batch_size=batch_size, on_epoch=True)
-
-        # Now we hook in part to compute confusion matrix.
-        for output, target in zip(outputs, targets):
+        for output, target in zip(model_outputs["outputs"], targets):
             if not target["class"]["valid"]:
                 continue
             self.probs.append(output["class"].cpu().numpy())
@@ -216,45 +136,20 @@ class ForestLossLightningModule(RslearnLightningModule):
         self.probs = []
         self.y_true = []
 
-    def test_step(self, batch: Any, batch_idx: int, dataloader_idx: int = 0) -> None:
-        """Compute test performance and also record for confusion matrix.
+    def on_test_forward(
+        self,
+        inputs: list[dict[str, Any]],
+        targets: list[dict[str, Any]],
+        model_outputs: dict[str, Any],
+    ) -> None:
+        """Hook to run after the forward pass of the model during testing.
 
         Args:
-            batch: the batch contents.
-            batch_idx: the batch index.
-            dataloader_idx: the index of the dataloader creating this batch.
+            inputs: The input batch.
+            targets: The target batch.
+            model_outputs: The output of the model, with keys "outputs" and "loss_dict", and possibly other keys.
         """
-        # Code below is copied from RslearnLightningModule.test_step.
-        inputs, targets, metadatas = batch
-        batch_size = len(inputs)
-        outputs, loss_dict = self(inputs, targets)
-        test_loss = sum(loss_dict.values())
-        self.log_dict(
-            {"test_" + k: v for k, v in loss_dict.items()},
-            batch_size=batch_size,
-            on_step=False,
-            on_epoch=True,
-        )
-        self.log(
-            "test_loss", test_loss, batch_size=batch_size, on_step=False, on_epoch=True
-        )
-        self.test_metrics.update(outputs, targets)
-        self.log_dict(self.test_metrics, batch_size=batch_size, on_epoch=True)
-
-        if self.visualize_dir:
-            for idx, (inp, target, output, metadata) in enumerate(
-                zip(inputs, targets, outputs, metadatas)
-            ):
-                images = self.task.visualize(inp, target, output)
-                for image_suffix, image in images.items():
-                    out_fname = os.path.join(
-                        self.visualize_dir,
-                        f"{metadata['window_name']}_{metadata['bounds'][0]}_{metadata['bounds'][1]}_{image_suffix}.png",
-                    )
-                    Image.fromarray(image).save(out_fname)
-
-        # Now we hook in part to compute confusion matrix.
-        for output, target in zip(outputs, targets):
+        for output, target in zip(model_outputs["outputs"], targets):
             if not target["class"]["valid"]:
                 continue
             self.probs.append(output["class"].cpu().numpy())
