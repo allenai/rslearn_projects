b3313858aaf584dd36b877d8c5d75cfa3538104b
Patrick Johnson <84832810+APatrickJ@users.noreply.github.com>
2025-08-20 22:50:30 -0700

    Add site name splits

---
+++ rslp/lfmc/assign_site_name_splits.py
@@ -0,0 +1,33 @@
+"""Assign split based on site name."""
+
+import hashlib
+import multiprocessing
+import sys
+
+import tqdm
+from upath import UPath
+
+from rslearn.dataset import Dataset, Window
+
+
+def assign_split(window: Window) -> None:
+    """Assign split of the window to train or val."""
+    site_name = window.options["site_name"]
+    is_val = hashlib.sha256(site_name.encode()).hexdigest()[0] in ["0", "1"]
+    if is_val:
+        split = "val"
+    else:
+        split = "train"
+    window.options["split"] = split
+    window.save()
+
+
+if __name__ == "__main__":
+    ds_path = UPath(sys.argv[1])
+    multiprocessing.set_start_method("forkserver")
+    windows = Dataset(ds_path).load_windows(workers=128, show_progress=True)
+    p = multiprocessing.Pool(128)
+    outputs = p.imap_unordered(assign_split, windows)
+    for _ in tqdm.tqdm(outputs, total=len(windows)):
+        pass
+    p.close()
