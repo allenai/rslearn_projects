a6af38b9c20b51c8fbf2c499e9fb69c152496281
Favyen Bastani <favyenb@allenai.org>
2025-08-21 12:48:33 -0700

    pass inference_result_data_type

--- rslp/esrun/esrun.py
+++ rslp/esrun/esrun.py
@@ -3,23 +3,28 @@
 from pathlib import Path
 
 from esrun.runner.local.predict_runner import EsPredictRunner
+from esrun.shared.models.task_results import InferenceResultsDataType
 
 from rslp.log_utils import get_logger
 
 logger = get_logger(__name__)
 
 
-def esrun(config_path: Path, scratch_path: Path) -> None:
+def esrun(
+    config_path: Path, scratch_path: Path, data_type: InferenceResultsDataType
+) -> None:
     """Run EsPredictRunner inference pipeline.
 
     Args:
         config_path: directory containing the model.yaml, partition_strategies.yaml,
             and postprocessing_strategies.yaml configuration files.
         scratch_path: directory to use for scratch space.
+        data_type: the output data type of the model.
     """
     runner = EsPredictRunner(
         project_path=config_path,
         scratch_path=scratch_path,
+        inference_results_data_type=data_type,
     )
     partitions = runner.partition()
     logger.info(f"Got {len(partitions)} partitions")
