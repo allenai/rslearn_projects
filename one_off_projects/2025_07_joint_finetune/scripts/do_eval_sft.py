5ef39ccdbad670f88c43f5969e05c06ca64e311e
ryanp <ryanp@allenai.org>
2025-08-29 20:33:18 +0000

    Update scripts

--- one_off_projects/2025_07_joint_finetune/scripts/do_eval_sft.py
+++ one_off_projects/2025_07_joint_finetune/scripts/do_eval_sft.py
@@ -7,11 +7,11 @@ import yaml
 base_dir = "/weka/dfive-default/ryanp/rslearn_projects/one_off_projects/2025_07_joint_finetune/configs"
 all_cfgs = {}
 for task in os.listdir(base_dir):
-    if task.startswith("v2_"):
+    if task.startswith("v2_") and task != "v2_landsat_vessels":
         all_cfgs[task] = [
             os.path.join(base_dir, task, cfg)
             for cfg in os.listdir(os.path.join(base_dir, task))
-            if cfg.endswith(".yaml")
+            if cfg.endswith(".yaml") and "soup" not in cfg
         ]
 
 all_cfgs["vessel_classification"] = [
@@ -29,6 +29,7 @@ parser.add_argument("ckpt_path", type=str, help="Path to the checkpoint")
 parser.add_argument("task", type=str, help="Task to evaluate")
 parser.add_argument("old_or_new", type=str, help="old or new helios checkpoint")
 parser.add_argument("--full", action="store_true", help="Run eval on all patches")
+parser.add_argument("--save_eval_path", type=str, help="Path to save eval results")
 args = parser.parse_args()
 
 ckpt_cfg_paths = all_cfgs[args.task]
@@ -38,7 +39,7 @@ else:
     helios_path = "/weka/dfive-default/helios/checkpoints/yawenzzzz/latent_mim_cross_random_per_modality_patchdisc_add_contrastive_0.1_1/step400000"
 print("using helios path", helios_path)
 print()
-cmd = [
+cmd_template = [
     "RSLP_PREFIX=/weka/dfive-default/rslearn-eai",
     "python", "-m", "rslp.main", "helios", "launch_finetune",
     "--helios_checkpoint_path", helios_path,
@@ -71,28 +72,48 @@ def load_yaml_config(config_path, substitutions=None):
                 config_str = config_str.replace(f"{{{key}}}", str(value))
     return yaml.safe_load(config_str)
 
-cmd.append(f"--ckpt_path={args.ckpt_path}")
-for ckpt_cfg_path in ckpt_cfg_paths:
-    with tempfile.NamedTemporaryFile(mode="w") as f:
-        cmd.append(f"--config_paths+={f.name}")
-        cfg = load_yaml_config(ckpt_cfg_path, substitutions)
+def deep_merge(base, override):
+    for k, v in override.items():
+        v_copy = v.copy() if hasattr(v, "copy") else v
+        if k.endswith("+"):
+            k = k[:-1]
+            if k not in base:
+                base[k] = []
+            base[k].extend(v_copy)
+        else:
+            if isinstance(v, dict):
+                base[k] = deep_merge(base.get(k, {}), v_copy)
+            else:
+                base[k] = v_copy
+    return base
 
-        if args.full:
-            cfg["data"]["init_args"]["use_in_memory_all_patches_dataset"] = True
-            for split in ["val", "test"]:
-                cfg["data"]["init_args"][f"{split}_config"]["load_all_patches"] = True
-                cfg["data"]["init_args"][f"{split}_config"]["patch_size"] = substitutions["PATCH_SIZE"]
+cmd_template.append(f"--ckpt_path={args.ckpt_path}")
+with tempfile.NamedTemporaryFile(mode="w") as f:
+    cmd = cmd_template.copy()
+    cmd.append(f"--config_paths+={f.name}")
+    if args.save_eval_path:
+        cmd.extend(["--save_eval_path", args.save_eval_path])
 
-        print("patched config to have in_memory_all_patches_dataset=True")
-        yaml.dump(cfg, f)
-        f.flush()
+    cfg = {}
+    for ckpt_cfg_path in ckpt_cfg_paths:
+        cfg = deep_merge(cfg, load_yaml_config(ckpt_cfg_path, substitutions))
 
-        print(f"Evaluating {args.task} with checkpoint {args.ckpt_path}")
-        print()
-        print("=" * 80)
-        print(" ".join(cmd))
-        print("=" * 80)
-        print()
+    if args.full:
+        cfg["data"]["init_args"]["use_in_memory_all_patches_dataset"] = True
+        for split in ["val", "test"]:
+            cfg["data"]["init_args"][f"{split}_config"]["load_all_patches"] = True
+            cfg["data"]["init_args"][f"{split}_config"]["patch_size"] = substitutions["PATCH_SIZE"]
 
-        os.chdir("/weka/dfive-default/ryanp/rslearn_projects/")
-        os.system(" ".join(cmd))
+    print("patched config to have in_memory_all_patches_dataset=True")
+    yaml.dump(cfg, f)
+    f.flush()
+
+    print(f"Evaluating {args.task} with checkpoint {args.ckpt_path}")
+    print()
+    print("=" * 80)
+    print(" ".join(cmd))
+    print("=" * 80)
+    print()
+
+    os.chdir("/weka/dfive-default/ryanp/rslearn_projects/")
+    os.system(" ".join(cmd))
--- one_off_projects/2025_07_joint_finetune/scripts/multiple_eval_sft.py
+++ one_off_projects/2025_07_joint_finetune/scripts/multiple_eval_sft.py
@@ -1,10 +1,16 @@
 import os
-import sys
+import argparse
 
-version = sys.argv[1]
-full = True
+parser = argparse.ArgumentParser()
+parser.add_argument("--version", type=str, required=True, help="options are [v1, v2, cosine]")
+parser.add_argument("--full", action="store_true", help="Run eval on all patches")
+parser.add_argument("--save_dir", type=str, help="Directory to save eval results", default="/weka/dfive-default/ryanp/rslearn_projects/one_off_projects/2025_07_joint_finetune/data/evals")
+args = parser.parse_args()
 
-ckpt_dir = "/weka/dfive-default/rslearn-eai/projects/2025_08_27_helios_cmp/"
+if args.version in ["v1", "v2"]:
+    ckpt_dir = "/weka/dfive-default/rslearn-eai/projects/2025_08_27_helios_cmp/"
+else:
+    ckpt_dir = "/weka/dfive-default/rslearn-eai/projects/helios_finetune_cosine_lr"
 ckpt = "checkpoints/last.ckpt"
 task_to_directory = {
     "v2_nandi_crop_type": "nandi_crop_type",
@@ -21,19 +27,23 @@ task_to_directory = {
 tasks = {v: k for k, v in task_to_directory.items()}
 
 for d in os.listdir(ckpt_dir):
-    if d.endswith("_v1") and version == "v1":
+    if d.endswith("_v1") and args.version == "v1":
         old_or_new = "old"
-    elif d.endswith("_v2") and version == "v2":
+    elif d.endswith("_v2") and args.version == "v2":
         old_or_new = "new"
-    else:
-        continue
+    elif args.version == "cosine":
+        old_or_new = "old"
 
     ckpt_path = os.path.join(ckpt_dir, d, ckpt)
-    task = tasks[d.replace("_" + version, "")]
+    try:
+        task = tasks[d.replace("_" + args.version, "")]
+    except KeyError:
+        continue
     cmd = f"python do_eval_sft.py {ckpt_path} {task} {old_or_new}"
-    if full:
+    if args.full:
         cmd += " --full"
+    if args.save_dir:
+        cmd += f" --save_eval_path {os.path.join(args.save_dir, d + '.json')}"
     print(cmd)
     os.system(cmd)
     print()
-    break
