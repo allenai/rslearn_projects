45e77f28d07426fca8ac04da53728a5815fba3c9
Patrick Johnson <84832810+APatrickJ@users.noreply.github.com>
2025-10-10 13:01:10 -0700

    Add LFMC configuration

---
+++ olmoearth_run_data/lfmc/env.sh
@@ -0,0 +1,7 @@
+# Run source env.sh to set the environment variables
+export DATASET_PATH=/weka/dfive-default/rslearn-eai/datasets/lfmc/20251011-scratch/dataset
+export EXTRA_FILES_PATH=/weka/dfive-default/helios/checkpoints
+export NUM_WORKERS=32
+export WANDB_ENTITY=eai-ai2
+export WANDB_NAME=lfmc_olmoearth_base_s1_s2_$(LC_ALL=C tr -dc 'a-z0-9' < /dev/urandom | head -c 4)
+export WANDB_PROJECT=$(date +%Y_%m_%d)_lfmc
