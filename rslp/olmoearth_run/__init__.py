72d26864cadc3ccd2d75ddf51fa35b74ce6888ff
Yawen Zhang <yawenz@allenai.org>
2025-10-10 13:35:55 -0700

    fix import

--- rslp/olmoearth_run/__init__.py
+++ rslp/olmoearth_run/__init__.py
@@ -1,9 +1,10 @@
 """olmoearth_run pipeline."""
 
-from .olmoearth_run import olmoearth_run, one_stage, prepare_labeled_windows
+from .olmoearth_run import finetune, olmoearth_run, one_stage, prepare_labeled_windows
 
 workflows = {
     "olmoearth_run": olmoearth_run,
     "one_stage": one_stage,
     "prepare_labeled_windows": prepare_labeled_windows,
+    "finetune": finetune,
 }
