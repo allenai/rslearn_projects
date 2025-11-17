54add44bad8fe8364486d584e403ffe69f37279b
Yawen Zhang <yawenz@allenai.org>
2025-11-17 15:07:12 -0800

    revert preemption

--- rslp/common/beaker_train.py
+++ rslp/common/beaker_train.py
@@ -194,7 +194,7 @@ def beaker_train(
                         constraints=BeakerConstraints(
                             cluster=cluster,
                         ),
-                        preemptible=False,  # TODO: set to True when we have enough GPUs
+                        preemptible=True,
                         datasets=datasets,
                         env_vars=env_vars,
                         resources=BeakerTaskResources(
