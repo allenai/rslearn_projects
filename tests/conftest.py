55aca81e7ee4decb711fa02a7f0335919dfcd29c
Favyen Bastani <favyenb@allenai.org>
2025-12-12 15:32:46 -0800

    need to call rslearn init_jsonargparse now for ResolutionFactor parsing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import logging
 
 import pytest
+from rslearn.utils.jsonargparse import init_jsonargparse
 
 from rslp.utils.mp import init_mp
 
@@ -10,3 +11,8 @@ logging.basicConfig()
 @pytest.fixture(scope="session", autouse=True)
 def always_init_mp() -> None:
     init_mp()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def always_init_jsonargparse() -> None:
+    init_jsonargparse()
