addc32696d89ce91117043bc1775f492dfdad2ad
root <root@phobos-cs-aus-453.reviz.ai2.in>
2026-01-14 19:59:26 +0000

    use spawn isntead

--- rslp/utils/mp.py
+++ rslp/utils/mp.py
@@ -6,25 +6,6 @@ import torch.multiprocessing
 
 
 def init_mp() -> None:
-    """Set start method to preload and configure forkserver preload."""
-    # Use file_system sharing to avoid passing 256+ FDs to forkserver
-    torch.multiprocessing.set_sharing_strategy("file_system")
-    multiprocessing.set_start_method("forkserver", force=True)
-    multiprocessing.set_forkserver_preload(
-        [
-            "pickle",
-            "fiona",
-            "gcsfs",
-            "jsonargparse",
-            "numpy",
-            "PIL",
-            "torch",
-            "torch.multiprocessing",
-            "torchvision",
-            "upath",
-            "wandb",
-            "rslearn.main",
-            "rslearn.train.dataset",
-            "rslearn.train.data_module",
-        ]
-    )
+    """Set start method to spawn and enforce file_system sharing."""
+    # torch.multiprocessing.set_sharing_strategy("file_system")
+    multiprocessing.set_start_method("spawn", force=True)
