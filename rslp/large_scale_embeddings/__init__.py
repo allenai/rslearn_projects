50939009eb184a1b309a48622097e3887c139ecf
Patrick Johnson <84832810+APatrickJ@users.noreply.github.com>
2026-08-14 09:02:55 -0700

    Add supervise workflow for preemption-resilient runs

--- rslp/large_scale_embeddings/__init__.py
+++ rslp/large_scale_embeddings/__init__.py
@@ -1,10 +1,13 @@
 """Global quantized OlmoEarth embedding inference."""
 
 from .predict_pipeline import predict_pipeline
+from .supervise import launch_supervisor, supervise
 from .write_jobs import init_store, write_jobs
 
 workflows = {
     "init_store": init_store,
+    "launch_supervisor": launch_supervisor,
     "predict": predict_pipeline,
+    "supervise": supervise,
     "write_jobs": write_jobs,
 }
